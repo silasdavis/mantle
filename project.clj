f678b5f773e87b29994723ff182de0a918e86d56
Silas Davis <silas@swiftkey.net>
2014-07-13 13:44:24 +0100

    downgrade clojure dependency for compatibility

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "https://github.com/silasdavis/mantle"
   :license {:name "MIT License"
             :url "https://github.com/silasdavis/mantle/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
